da4a504e93dd84096cdc40152c648d11f0b2ba37
Nikita <nikita.gubarev31@gmail.com>
2023-03-16 09:21:38 +0200

    Replace js sessionStorage with localStorage in toggle_theme.js

--- static/js/toggle_theme.js
+++ static/js/toggle_theme.js
@@ -2,17 +2,17 @@ const theme_toggler_btn = document.getElementById("theme_toggler_btn");
 
 theme_toggler_btn.addEventListener("change", () => {
 	if (document.documentElement.hasAttribute("theme")) {
-		sessionStorage.setItem("dark-theme", "no");
+		localStorage.setItem("dark-theme", "no");
 		document.documentElement.removeAttribute("theme");
 	} else set_dark_mode();
 });
 
 function set_dark_mode() {
-	sessionStorage.setItem("dark-theme", "yes");
+	localStorage.setItem("dark-theme", "yes");
 	document.documentElement.setAttribute("theme", "dark");
 }
 
-if (sessionStorage.getItem("dark-theme") == "yes") {
+if (localStorage.getItem("dark-theme") == "yes") {
 	set_dark_mode();
 	theme_toggler_btn.checked = !theme_toggler_btn.checked;
 }
